c5505eb5a7f86ed4bd95fa355af59a34df071e61
Tasha Hankewych <tash1207@bellsouth.net>
2020-06-07 19:33:37 -0700

    Add simple touch event drawing functionality.

---
+++ app/src/main/java/co/tashawych/hobbies/DrawingCanvas.java
@@ -0,0 +1,71 @@
+package co.tashawych.hobbies;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class DrawingCanvas extends View {
+
+    private Paint canvasPaint, drawPaint;
+    private Path drawPath;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
+    public DrawingCanvas(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        setUp();
+    }
+
+    private void setUp() {
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+        drawPaint = new Paint();
+        drawPaint.setColor(0xFF0000AA);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        drawPath = new Path();
+    }
+
+    @Override
+    protected void onSizeChanged(int width, int height, int oldWidth, int oldHeight) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight);
+        canvasBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        invalidate();
+        return true;
+    }
+}

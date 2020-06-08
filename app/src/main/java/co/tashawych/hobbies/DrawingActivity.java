52e02401273ec8a03fdf01f98f82b7f1f44f8171
Tasha Hankewych <tash1207@bellsouth.net>
2020-06-07 18:35:44 -0700

    Add button that opens new drawing activity.

---
+++ app/src/main/java/co/tashawych/hobbies/DrawingActivity.java
@@ -0,0 +1,14 @@
+package co.tashawych.hobbies;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrawingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawing);
+    }
+}
--- app/src/main/java/co/tashawych/hobbies/MainActivity.java
+++ app/src/main/java/co/tashawych/hobbies/MainActivity.java
@@ -1,8 +1,10 @@
 package co.tashawych.hobbies;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openDrawingActivity(View view) {
+        Intent drawingActivityIntent = new Intent(getApplicationContext(), DrawingActivity.class);
+        startActivity(drawingActivityIntent);
     }
 }
